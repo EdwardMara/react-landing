e87f17ae2c0ebca84b55c551fb3f54263a6dcc60
Edward Mara <e.james.mara@gmail.com>
2019-11-04 13:12:07 -0800

    declared searchTerm and searchUrl initial states

--- src/App.js
+++ src/App.js
@@ -2,25 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var App = React.createClass({
+  getInitialState: function () {
+    return { searchTerm: "", searchUrl: "" };
+  },
+  handleKeyUp: function (e) {
+    if (e.key === 'Enter' && this.state.searchTerm !== '') {
+      var searchUrl = 
+      this.setState({ searchUrl: searchUrl });
+    }
+  }
 }
+)
 
 export default App;
