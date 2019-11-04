077182bdac7062d84854b7b59740736c30824225
Edward Mara <e.james.mara@gmail.com>
2019-11-04 13:38:23 -0800

    componentDidMount function with loadContent function that exectues fetch

--- src/App.js
+++ src/App.js
@@ -8,11 +8,39 @@ var App = React.createClass({
   },
   handleKeyUp: function (e) {
     if (e.key === 'Enter' && this.state.searchTerm !== '') {
-      var searchUrl = 
-      this.setState({ searchUrl: searchUrl });
+      var searchUrl = //full url with query + searchTerm + APIkey
+        this.setState({ searchUrl: searchUrl });
     }
   }
 }
-)
+);
+
+var TitleList = React.createClass({
+  apiKey: //api key here,
+    
+  getInitialState: function() {
+    return { data: [], mounted: false };
+  },
+
+  loadContent: function () {
+    var requestUrl = //moviedbapi url + this.props.url+ '&api_key=' +APIkey;
+      fetch(requestUrl).then((response) => {
+        return response.json();
+      }).then((data) => {
+        this.setState({ data: data });
+      }).catch((err) => {
+        console.log("Error Caught");
+      });
+  
+  },
+
+
+  componentDidMount: function () {
+    if (this.props.orl !== '') {
+      this.loadContent();
+      this.setState({mounted:true})
+    }
+  },
+})
 
 export default App;
